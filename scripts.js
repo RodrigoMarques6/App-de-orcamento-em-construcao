23fb0204644a1b250f4f07dfd9978fa8fc44e1de
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2023-03-16 11:07:26 -0300

    App-de-orcamento-em-construcao

--- scripts.js
+++ scripts.js
@@ -6,4 +6,49 @@ const inputEconomico = document.getElementById('input-economico');
 const inputAntecipado = document.getElementById('input-antecipado');
 const btnEconomico = document.getElementById('btn-economico');
 const btnAntecipado = document.getElementById('btn-antecipado');
-const h2Main = document.getElementById('h2-main');
+
+
+
+btnEconomico.onclick = function createTableOfValues() {
+
+
+
+    retiraDivEconomico()
+}
+
+function retiraDivEconomico () {
+    const divEconomico = document.getElementById('div-economico')
+    // divEconomico.style.display = 'none'
+    divEconomico.innerHTML = `<table>
+    <thead>
+        <th>Modalidade</th>
+        <th>Taxa</th>
+        <th>Você recebe</th>
+        <th>Para o cliente</th>
+
+    </thead>
+    <tbody>
+        <td>Débito</td>
+        <td>1,90%</td>
+        <td value=""></td>
+        <td value=""></td>
+        <td value=""></td>
+    </tbody>
+    <tfoot></tfoot><table>
+    <thead>
+        <th>Modalidade</th>
+        <th>Taxa</th>
+        <th>Você recebe</th>
+        <th>Para o cliente</th>
+
+    </thead>
+    <tbody>
+        <td>Débito</td>
+        <td>1,90%</td>
+        <td value=""></td>
+        <td value=""></td>
+        <td value=""></td>
+    </tbody>
+    <tfoot></tfoot>`
+}
+
