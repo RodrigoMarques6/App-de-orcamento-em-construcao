538bfb055d66110be9443aa8ee00ca01d3d41ff6
Aluno noite <alunonoite@senai.ti>
2023-03-15 21:14:30 -0300

    App-de-orcamento-em-construcao

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM funcionando perfeitamente.')
 })
+
+const inputEconomico = document.getElementById('input-economico');
+const inputAntecipado = document.getElementById('input-antecipado');
+const btnEconomico = document.getElementById('btn-economico');
+const btnAntecipado = document.getElementById('btn-antecipado');
+const h2Main = document.getElementById('h2-main');
